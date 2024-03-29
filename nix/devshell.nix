fd0b740255b409f5c3b57901206044e4ebc3e170
Mika Tammi <mikatammi@gmail.com>
2024-03-29 15:08:45 +0200

    Add devShell with reuse
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -1,6 +1,12 @@
 # SPDX-FileCopyrightText: 2024 Mika Tammi
 # SPDX-License-Identifier: MIT
 _: {
-  perSystem = _: {
+  perSystem = {pkgs, ...}: {
+    devShells.default = pkgs.mkShell {
+      name = "devShell mostly for convenience during Github Actions";
+      packages = with pkgs; [
+        reuse
+      ];
+    };
   };
 }
